16a61e7ea3ff64b47955b4c4240a003d83ed7aff
GuidoAsbun <guido@asbun.io>
2021-12-05 16:45:55 -0800

    Working on Main.cpp, not complete

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 #include "DonorList.h"
+#include "Interface.h"
+#include "InputHandler.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Test Main" << endl;
+    DonorList aDonorList;
+    getDonorData(aDonorList);
+    if (!aDonorList.isEmpty())
+    {
+        displayMenu();
+        processSelection(aDonorList);
+    }
+
     return 0;
 }
